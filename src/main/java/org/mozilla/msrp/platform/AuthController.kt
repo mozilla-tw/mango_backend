c9c588daa57108bcf335ec1dc9bfa739b30beda3
Nevin Chen <cnevinchen@gmail.com>
2019-08-01 15:53:25 +0800

    Remove UserDoc when fxuid exist, or update uid to fxuid when promote

--- src/main/java/org/mozilla/msrp/platform/AuthController.kt
+++ src/main/java/org/mozilla/msrp/platform/AuthController.kt
@@ -1,6 +1,9 @@
 package org.mozilla.msrp.platform
 
+import com.google.cloud.firestore.CollectionReference
+import com.google.cloud.firestore.SetOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.cloud.FirestoreClient
 import org.json.JSONException
 import org.json.JSONObject
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController
 
 import javax.servlet.http.HttpServletResponse
 import java.io.IOException
-import java.util.HashMap
 
 import org.springframework.web.bind.annotation.RequestMethod.GET
+import java.text.SimpleDateFormat
+import java.util.*
 
 @RestController
 class AuthController {
@@ -28,40 +32,44 @@ class AuthController {
 
         // read system settings
         val fxAclientId = PlatformApplication.FXA_CLIENT_ID
-        val fxAtokenEp = PlatformApplication.FXA_EP_TOKEN
-        val fxAverifyEp = PlatformApplication.FXA_EP_VERIFY
+        val fxTokenEp = PlatformApplication.FXA_EP_TOKEN
+        val fxVerifyEp = PlatformApplication.FXA_EP_VERIFY
         val fxAsecret = PlatformApplication.FXA_CLIENT_SECRET
 
 
         try {
-            // token: code -> token
-            val fxaTokenJson = JSONObject()
+            // token: fxCode -> fxToken
+            val fxTokenJson = JSONObject()
                     .put("client_id", fxAclientId)
                     .put("grant_type", "authorization_code")
                     .put("ttl", 3600)
                     .put("client_secret", fxAsecret)
                     .put("code", code)
-            println("fxaTokenJson===$fxaTokenJson")
-            val fxAtokenRes = HttpUtil.post(fxAtokenEp, fxaTokenJson)
-            println("fxAtokenRes===$fxAtokenRes")
-            val fxJsonObject = JSONObject(fxAtokenRes)
-            val fxAtoken = fxJsonObject.getString("access_token")
+            println("fxTokenJson===$fxTokenJson")
+            val fxTokenRes = HttpUtil.post(fxTokenEp, fxTokenJson)
+            println("fxTokenRes===$fxTokenRes")
 
-            // verify: token -> uid
+            val fxJsonObject = JSONObject(fxTokenRes)
+            val fxToken = fxJsonObject.getString("access_token")
+            println("fxToken===$fxToken")
+
+            // verify: token -> fxuid
             val verifyJson = JSONObject().run {
-                put("token", fxAtoken)
+                put("token", fxToken)
             }
-            val fxVerifyRes = HttpUtil.post(fxAverifyEp, verifyJson)
-            println("fxVerifyRes===$fxVerifyRes")
+            val fxVerifyRes = HttpUtil.post(fxVerifyEp, verifyJson)
+            val fxUid = JSONObject(fxVerifyRes).getString("user")
+            println("fxUid===$fxUid")
+
+            promoteUserDocument(oldFbUid, fxUid)
 
             // create custom token (jwt) for Firebase client SDK
             val additionalClaims = HashMap<String, Any>()
-            val fxUid = JSONObject(fxVerifyRes).getString("user")
             additionalClaims["fxuid"] = fxUid
             additionalClaims["oldFbUid"] = oldFbUid
             val auth = FirebaseAuth.getInstance()
             val customToken = auth.createCustomToken(fxUid, additionalClaims)
-            httpResponse.sendRedirect("/done?jwt=$customToken&at=$fxAtoken&email=nechen@mozilla.com")
+            httpResponse.sendRedirect("/done?jwt=$customToken&at=$fxToken&email=nechen@mozilla.com")
 
             return "done"
 
@@ -71,5 +79,46 @@ class AuthController {
         }
     }
 
+    private fun promoteUserDocument(oldFbUid: String, fxUid: String) {
+        val db = FirestoreClient.getFirestore()
+        val users = db.collection("users")
 
+        // if there's an user document with uid == fxuid , remove current one
+        val fxUserDocumentId = findUserDocumentIdByUid(users, fxUid)
+        println("fxUserDocumentId======$fxUserDocumentId")
+
+        if (fxUserDocumentId != null) {
+            val dying = findUserDocumentIdByUid(users, oldFbUid)
+            println("dying======$dying")
+
+            if (dying != null) {
+                users.document(dying).delete()
+                return
+            }
+        }
+
+        val documentId = findUserDocumentIdByUid(users, oldFbUid) ?: return
+
+        println("oldFbUid======$oldFbUid")
+        println("oldFbUid======$fxUid")
+        println("documentId======$documentId")
+
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        val docData = HashMap<String, Any>()
+        docData["isfxa"] = true
+        docData["uid"] = fxUid
+        docData["update_date"] = currentDate
+        docData["update_reason"] = "sign_in_fxa"
+        users.document(documentId).set(docData, SetOptions.merge())
+    }
+
+    private fun findUserDocumentIdByUid(users: CollectionReference, fxUid: String): String? {
+        val documents = users.whereEqualTo("uid", fxUid).get().get().documents
+        if (documents.size >= 1) {
+            return documents[0].id
+        } else {
+            return null
+        }
+    }
 }
