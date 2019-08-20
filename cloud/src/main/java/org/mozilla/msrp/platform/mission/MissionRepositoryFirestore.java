53d78e1bec803f5c63fed28b1612e1a7a87266ea
Roger <pharspher@gmail.com>
2019-08-21 02:50:35 +0800

    Implement MissionRepository

---
+++ cloud/src/main/java/org/mozilla/msrp/platform/mission/MissionRepositoryFirestore.java
@@ -0,0 +1,72 @@
+package org.mozilla.msrp.platform.mission;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+@Named
+public class MissionRepositoryFirestore implements MissionRepository {
+
+    private Firestore firestore;
+
+    @Inject
+    MissionRepositoryFirestore(Firestore firestore) {
+        this.firestore = firestore;
+    }
+
+    @Override
+    public List<RawMission> getMissionsByGroupId(String groupId) {
+        return getMissionRefsByGroupId(groupId).stream()
+                .map(this::getMissionsByRef)
+                .flatMap(Collection::stream)
+                .map(this::convertToRawMission)
+                .filter(Optional::isPresent).map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private List<QueryDocumentSnapshot> getMissionRefsByGroupId(String groupId) {
+        return getQueryResult(firestore.collection(groupId));
+    }
+
+    private List<QueryDocumentSnapshot> getMissionsByRef(QueryDocumentSnapshot referenceDoc) {
+        String type = referenceDoc.getString("type");
+        String mid = referenceDoc.getString("mid");
+
+        if (type == null || mid == null) {
+            return new ArrayList<>();
+        }
+
+        return getQueryResult(firestore.collection(type).whereEqualTo("mid", mid));
+    }
+
+    private Optional<RawMission> convertToRawMission(QueryDocumentSnapshot missionDoc) {
+        String mid = missionDoc.getString(RawMission.KEY_MID);
+        String nameId = missionDoc.getString(RawMission.KEY_NAME_ID);
+        String descriptionId = missionDoc.getString(RawMission.KEY_DESCRIPTION_ID);
+
+        if (mid == null || nameId == null || descriptionId == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new RawMission(mid, nameId, descriptionId));
+    }
+
+    private List<QueryDocumentSnapshot> getQueryResult(Query query) {
+        try {
+            return query.get().get().getDocuments();
+
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
---
+++ cloud/src/main/java/org/mozilla/msrp/platform/mission/RawMission.kt
@@ -0,0 +1,17 @@
+package org.mozilla.msrp.platform.mission
+
+/**
+ * (All fields are just draft and are subject to change)
+ * Mission retrieved from persistent layer
+ */
+data class RawMission(
+        val mid: String,
+        val nameId: String,
+        val descriptionId: String
+) {
+    companion object {
+        const val KEY_MID = "mid"
+        const val KEY_NAME_ID = "nameId"
+        const val KEY_DESCRIPTION_ID = "descriptionId"
+    }
+}
