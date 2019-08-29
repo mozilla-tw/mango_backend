959c2679d53418f213b8c408af92bfc93ebcad3c
Nevin Chen <cnevinchen@gmail.com>
2019-08-29 22:11:27 +0800

    Return error message when HandlerInterceptor can't verify users.

--- src/main/java/org/mozilla/msrp/platform/common/auth/FireBaseAuthConfiguration.java
+++ src/main/java/org/mozilla/msrp/platform/common/auth/FireBaseAuthConfiguration.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.inject.Inject;
+
 @Configuration
 public class FireBaseAuthConfiguration implements WebMvcConfigurer {
+
+    private FirebaseAuthInterceptor firebaseAuthInterceptor;
+
+
+    @Inject
+    public FireBaseAuthConfiguration(FirebaseAuthInterceptor firebaseAuthInterceptor) {
+        this.firebaseAuthInterceptor = firebaseAuthInterceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new FirebaseAuthInterceptor()).addPathPatterns("/login/**");
-        registry.addInterceptor(new FirebaseAuthInterceptor()).addPathPatterns("/done");
+        registry.addInterceptor(this.firebaseAuthInterceptor).addPathPatterns("/redeem/**", "/done");
     }
 }
