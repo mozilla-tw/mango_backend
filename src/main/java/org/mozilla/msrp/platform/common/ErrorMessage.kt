7b02b60384d20e66f8e9c4ea7e55c892e5bc5cbb
Nevin Chen <cnevinchen@gmail.com>
2019-08-29 22:11:27 +0800

    Return error message when HandlerInterceptor can't verify users.

---
+++ src/main/java/org/mozilla/msrp/platform/common/ErrorMessage.kt
@@ -0,0 +1,9 @@
+package org.mozilla.msrp.platform.common
+
+import java.util.Date
+
+
+class ErrorMessage(
+    val timestamp: Date,
+    val message: String
+)
--- src/main/java/org/mozilla/msrp/platform/common/auth/FirebaseAuthInterceptor.java
+++ src/main/java/org/mozilla/msrp/platform/common/auth/FirebaseAuthInterceptor.java
@@ -1,14 +1,24 @@
 package org.mozilla.msrp.platform.common.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.extern.log4j.Log4j2;
+import org.mozilla.msrp.platform.common.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 @Log4j2
+@Named
 public class FirebaseAuthInterceptor implements HandlerInterceptor {
 
     private static String HEADER_BEAR = "Bear ";
@@ -19,20 +29,46 @@ public class FirebaseAuthInterceptor implements HandlerInterceptor {
         String authorization = request.getHeader("Authorization");
 
         if (authorization != null && authorization.contains(HEADER_BEAR)) {
+
             String jwt = authorization.replace(HEADER_BEAR, "");
+
             try {
                 FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(jwt);
+
                 if (decodedToken.getUid().isEmpty()) {
+
+                    handleThrowable(response, HttpStatus.UNAUTHORIZED, "No such user");
+
                     return false;
                 }
-            } catch (Exception e) {
+            } catch (Throwable throwable) {
+
+                handleThrowable(response, HttpStatus.INTERNAL_SERVER_ERROR, "Error loading DB");
+
                 return false;
             }
             return true;
+
         } else {
+
             log.info("abnormal access to endpoint: {}", request.getRequestURI());
+
+            handleThrowable(response, HttpStatus.UNAUTHORIZED, "Please login first");
+
+            return false;
         }
-        return false;
+    }
+
+    private void handleThrowable(HttpServletResponse response, HttpStatus httpStatus, String message) throws IOException {
+        ErrorMessage errorObj = new ErrorMessage(new Date(), message);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonMessage = mapper.writeValueAsString(errorObj);
+        response.addHeader(HttpHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON_VALUE);
+        response.setStatus(httpStatus.value());
+        PrintWriter writer = response.getWriter();
+        writer.write(jsonMessage);
+        writer.flush();
+        writer.close();
     }
 
 }
