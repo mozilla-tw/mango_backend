4817f6f2e767797d0de5d6a86db1f5d95ef8a6c7
Roger <pharspher@gmail.com>
2019-09-05 15:57:28 +0800

    Add some extension functions for Firestore manipulation (#27)

--- src/main/java/org/mozilla/msrp/platform/firestore/FirestoreExt.kt
+++ src/main/java/org/mozilla/msrp/platform/firestore/FirestoreExt.kt
@@ -1,6 +1,7 @@
 package org.mozilla.msrp.platform.firestore
 
-import com.google.cloud.firestore.DocumentSnapshot
+import com.google.cloud.firestore.*
+import org.mozilla.msrp.platform.mission.getUnchecked
 
 fun DocumentSnapshot.areFieldsPresent(fieldNames: List<String>): Boolean {
     fieldNames.forEach { fieldName ->
@@ -8,3 +9,26 @@ fun DocumentSnapshot.areFieldsPresent(fieldNames: List<String>): Boolean {
     }
     return true
 }
+
+/** Extensions for read/write from/to Firestore */
+
+fun Query.getResultsUnchecked(): List<QueryDocumentSnapshot> {
+   return get().getUnchecked().documents
+}
+
+fun DocumentReference.getUnchecked(): DocumentSnapshot {
+    return get().getUnchecked()
+}
+
+fun DocumentReference.setUnchecked(obj: Any): WriteResult {
+    return set(obj).getUnchecked()
+}
+
+
+/** Extensions for Collection/Document navigation */
+
+val DocumentReference.parentCollection
+    get() = this.parent
+
+val CollectionReference.parentDocument: DocumentReference?
+    get() = this.parent
