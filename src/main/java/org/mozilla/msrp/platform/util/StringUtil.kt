59ba03c72c5fd914308f1feeb62a4de8c7887736
Nevin Chen <cnevinchen@gmail.com>
2019-09-09 19:51:30 +0800

    Add shopping

---
+++ src/main/java/org/mozilla/msrp/platform/util/StringUtil.kt
@@ -0,0 +1,10 @@
+package org.mozilla.msrp.platform.util
+
+import java.security.MessageDigest
+
+fun String.hash(): String {
+    val bytes = this.toByteArray()
+    val md = MessageDigest.getInstance("SHA-256")
+    val digest = md.digest(bytes)
+    return digest.fold("", { str, it -> str + "%02x".format(it) })
+}
