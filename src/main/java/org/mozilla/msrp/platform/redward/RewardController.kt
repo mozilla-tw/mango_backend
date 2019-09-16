ec42bd28a6275dbc84fc5f7d4b04cea56a5f0950
Nevin Chen <cnevinchen@gmail.com>
2019-09-16 17:59:38 +0800

    Fix per comments

--- src/main/java/org/mozilla/msrp/platform/redward/RewardController.kt
+++ src/main/java/org/mozilla/msrp/platform/redward/RewardController.kt
@@ -1,11 +1,9 @@
 package org.mozilla.msrp.platform.redward
 
+import org.mozilla.msrp.platform.util.logger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestAttribute
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 
 
@@ -21,24 +19,31 @@ sealed class RedeemResponse {
 @RestController
 class RedeemController @Inject constructor(val rewardRepository: RewardRepository) {
 
-
+    private var logger = logger()
     /**
      * Get redeem code if possible.
      * Return the failing reason from the service.
      *
      * */
-    @RequestMapping("/api/v1/reward")
-    internal fun redeem(@RequestParam(value = "mid") mid: String,
+    @RequestMapping("/api/v1/redeem/{missionType}")
+    internal fun redeem(@PathVariable("missionType") missionType: String,
+                        @RequestParam(value = "mid") mid: String,
                         @RequestAttribute("uid") uid: String): ResponseEntity<RedeemResponse>? {
+        val redeemResult = rewardRepository.redeem(missionType, mid, uid)
+
+        if (redeemResult == null) {
+            logger.error("Unexpected error when redeem: Type for missionType[$missionType] mid[$mid] uid[$uid]")
+        } else {
+            logger.info(redeemResult.debugInfo)
+        }
 
         // TODO: add logging
-        return when (val redeemResult = rewardRepository.redeem(mid, uid)) {
+        return when (redeemResult) {
             is RedeemResult.Success -> ResponseEntity(RedeemResponse.Success(redeemResult.rewardCouponDoc), HttpStatus.OK)
             is RedeemResult.UsedUp -> ResponseEntity(RedeemResponse.Fail("No reward left"), HttpStatus.NOT_FOUND)
-            is RedeemResult.NotReady -> ResponseEntity(RedeemResponse.Fail("Not ready to redeem"), HttpStatus.BAD_REQUEST)
+            is RedeemResult.NotReady -> ResponseEntity(RedeemResponse.Fail("Not ready to redeem"), HttpStatus.FORBIDDEN)
+            is RedeemResult.InvalidRewardType -> ResponseEntity(RedeemResponse.Fail("Invalid reward type"), HttpStatus.BAD_REQUEST)
             else -> ResponseEntity(RedeemResponse.Fail("Not able to redeem"), HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
-
 }
--- src/main/java/org/mozilla/msrp/platform/redward/RewardRepository.kt
+++ src/main/java/org/mozilla/msrp/platform/redward/RewardRepository.kt
@@ -1,10 +1,9 @@
 package org.mozilla.msrp.platform.redward
 
 import com.google.api.core.ApiFuture
+import com.google.cloud.firestore.DocumentReference
 import com.google.cloud.firestore.Firestore
 import com.google.cloud.firestore.Transaction
-import lombok.extern.log4j.Log4j2
-import org.mozilla.msrp.platform.firestore.getResultsUnchecked
 import org.mozilla.msrp.platform.firestore.getUnchecked
 import org.mozilla.msrp.platform.mission.JoinStatus
 import org.mozilla.msrp.platform.mission.MissionJoinDoc
@@ -13,21 +12,22 @@ import java.time.Clock
 import javax.inject.Inject
 import javax.inject.Named
 
-sealed class RedeemResult() {
-    class Success(val rewardCouponDoc: RewardCouponDoc) : RedeemResult()
-    class UsedUp(val message: String) : RedeemResult()
-    class NotReady(val message: String) : RedeemResult()
-    class Failure(val message: String) : RedeemResult()
+sealed class RedeemResult(val debugInfo: String) {
+    class Success(val rewardCouponDoc: RewardCouponDoc, debugMessage: String) : RedeemResult(debugMessage)
+    class UsedUp(val message: String, debugMessage: String) : RedeemResult(debugMessage)
+    class NotReady(val message: String, debugMessage: String) : RedeemResult(debugMessage)
+    class InvalidRewardType(val message: String, debugMessage: String) : RedeemResult(debugMessage)
+    class Failure(val message: String, debugMessage: String) : RedeemResult(debugMessage)
 }
 
 @Named
-@Log4j2
 class RewardRepository @Inject constructor(
 
-    private val missionRepository: MissionRepository,
-    private val firestore: Firestore) {
+        private val missionRepository: MissionRepository,
+        private val firestore: Firestore) {
 
-    private val clock: Clock = Clock.systemUTC()
+    @Inject
+    lateinit var clock: Clock
 
     /**
      * Return a Reward document for ther user.
@@ -38,11 +38,13 @@ class RewardRepository @Inject constructor(
      *
      * @return RedeemResult
      * */
-    fun redeem(mid: String, uid: String): RedeemResult? {
+    fun redeem(missionType: String, mid: String, uid: String): RedeemResult? {
 
-        //TODO: remove hard code , we only have daily mission
-        val missionType = "mission_daily"       // get the mission type from client?
-        val rewardType = "reward_coupon"        // get the rewardType from mission_daily document?
+        val rewardType: String = missionRepository.findMission(missionType, mid)?.rewardType
+                ?: // should be "reward_coupon"
+                return RedeemResult.InvalidRewardType(
+                        "Can't find Reward Type",
+                        "Can't find Reward Type for missionType[$missionType] mid[$mid] uid[$uid]")
 
         // TODO: extract all firebase related class here, and move business logic to a domain class
         val trans: ApiFuture<RedeemResult> = firestore.runTransaction { transaction ->
@@ -52,42 +54,47 @@ class RewardRepository @Inject constructor(
             when (missionJoinDoc?.status) {
                 JoinStatus.Redeemed -> {
                     val rewardDocId = missionJoinDoc.rewardDocId
-                        ?: // shouldn't happen, data error
-                        return@runTransaction RedeemResult.Failure("MissionJoinDoc's status is Redeemed, but RewardDocId is null ${info(missionJoinDoc)}")
+                            ?: // shouldn't happen, data error
+                            return@runTransaction RedeemResult.Failure(
+                                    "Reward format invalid",
+                                    "MissionJoinDoc 's status is Redeemed, but RewardDocId is null ${info(missionJoinDoc)}")
 
                     // return its related RewardDoc, currently we only have Coupon-Reward
                     val rewardCouponDoc = getRewardDoc(rewardType, rewardDocId)
-                        ?: // shouldn't happen, data error
-                        return@runTransaction RedeemResult.Failure("Found RewardDocId $rewardDocId but no Doc is found ${info(missionJoinDoc)}")
+                            ?: // shouldn't happen, data error
+                            return@runTransaction RedeemResult.Failure("Can't find your reward", "Found RewardDocId $rewardDocId but no Doc is found ${info(missionJoinDoc)}")
 
-                    return@runTransaction RedeemResult.Success(rewardCouponDoc)
+                    return@runTransaction RedeemResult.Success(rewardCouponDoc, "Reward doc ")
                 }
 
                 JoinStatus.Complete -> {
 
                     // update RewardDoc
-                    val rewardDocId = requestReward(rewardType)?.rid
-                        ?: // shouldn't happen, data error
-                        return@runTransaction RedeemResult.UsedUp("No reward left ${info(missionJoinDoc)}")
-                    consumeRewardDoc(rewardType, uid, mid, rewardDocId, transaction)
+                    val rewardDocId = requestReward(rewardType, transaction)?.rid
+                            ?: // shouldn't happen, data error
+                            return@runTransaction RedeemResult.UsedUp(
+                                    "No reward left",
+                                    "No reward left ${info(missionJoinDoc)}")
+
+                    val rewardDoc = consumeRewardDoc(rewardType, uid, mid, rewardDocId, transaction)
 
                     // update MissionJoinDoc
                     val success = missionRepository.updateMissionJoinDocAfterRedeem(uid, missionType, mid, rewardDocId, transaction)
-                    if (success) {
+                    if (success && rewardDoc != null) {
 
-                        // query again and return the rewardDoc
-                        val rewardDocAgain = getRewardDoc(rewardType, rewardDocId)
-                            ?: // shouldn't happen, data error
-                            return@runTransaction RedeemResult.Failure("Can't find RewardDoc after update ${info(missionJoinDoc)}")
-                        return@runTransaction RedeemResult.Success(rewardDocAgain)
+                        return@runTransaction RedeemResult.Success(rewardDoc, "Reward consumed! ${info(missionJoinDoc)}")
 
                     } else {
-                        return@runTransaction RedeemResult.Failure("Can't update MissionJoinDoc ${info(missionJoinDoc)}")
+                        return@runTransaction RedeemResult.Failure(
+                                "Can't update Mission Progress",
+                                "Can't update MissionJoinDoc ${info(missionJoinDoc)}")
                     }
                 }
                 else -> {
                     // mission in state other than `Completed` or `Redeemed` are not eligible for redeem
-                    return@runTransaction RedeemResult.NotReady("Not ready to redeem for mission ${missionJoinDoc?.mid}, user ${missionJoinDoc?.uid}\"")
+                    return@runTransaction RedeemResult.NotReady(
+                            "Not ready to redeem",
+                            "Not ready to redeem for mission ${missionJoinDoc?.mid}, user ${missionJoinDoc?.uid}")
                 }
             }
         }
@@ -95,34 +102,43 @@ class RewardRepository @Inject constructor(
     }
 
     private fun info(missionJoinDoc: MissionJoinDoc) =
-        "[INFO: mission ${missionJoinDoc.mid}, user ${missionJoinDoc.uid}]"
+            "[INFO: mission ${missionJoinDoc.mid}, user ${missionJoinDoc.uid}]"
 
 
     // consume that reward with user and mission ids
-    private fun consumeRewardDoc(rewardType: String, uid: String, mid: String, docId: String, transaction: Transaction) {
+    private fun consumeRewardDoc(rewardType: String, uid: String, mid: String, docId: String, transaction: Transaction): RewardCouponDoc? {
         val document = firestore.collection(rewardType)
-            .document(docId)
+                .document(docId)
+        val toEpochMilli = clock.instant().toEpochMilli()
         transaction.update(document, mapOf(
-            "uid" to uid,
-            "mid" to mid,
-            "updated_timestamp" to clock.instant().toEpochMilli()
+                "uid" to uid,
+                "mid" to mid,
+                "updated_timestamp" to toEpochMilli
         ))
+        val rewardCouponDoc = document.getUnchecked().toObject(RewardCouponDoc::class.java)
+        rewardCouponDoc?.uid = uid
+        rewardCouponDoc?.mid = mid
+        rewardCouponDoc?.updated_timestamp = toEpochMilli
+
+        return rewardCouponDoc
     }
 
     // find a reward that has no user bind to it for that reward type.
-    private fun requestReward(rewardType: String): RewardCouponDoc? {
-        return firestore.collection(rewardType)
-            .whereEqualTo("uid", "")
-            .limit(1)
-            .getResultsUnchecked().getOrNull(0)
-            ?.toObject(RewardCouponDoc::class.java)
+    private fun requestReward(rewardType: String, transaction: Transaction): RewardCouponDoc? {
+
+        val query = firestore.collection(rewardType)
+                .whereEqualTo("uid", "")
+                .limit(1)
+        transaction.mutationsSize
+        return transaction.get(query)
+                .getUnchecked().documents.getOrNull(0)?.toObject(RewardCouponDoc::class.java)
     }
 
     private fun getRewardDoc(couponType: String, docId: String): RewardCouponDoc? {
         return firestore.collection(couponType)
-            .document(docId)
-            .getUnchecked()
-            .toObject(RewardCouponDoc::class.java)
+                .document(docId)
+                .getUnchecked()
+                .toObject(RewardCouponDoc::class.java)
     }
 
 }
