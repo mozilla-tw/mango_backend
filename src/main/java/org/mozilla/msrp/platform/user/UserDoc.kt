d06d9b7955e39d340dea18cfc9c68988c1ddb62e
Nevin Chen <cnevinchen@gmail.com>
2019-09-18 01:13:48 +0800

    Bunch rename

--- src/main/java/org/mozilla/msrp/platform/user/UserDoc.kt
+++ src/main/java/org/mozilla/msrp/platform/user/UserDoc.kt
@@ -1,6 +1,17 @@
 package org.mozilla.msrp.platform.user.data
 
 
+data class UserActivityDoc(
+        val userDocId: String,
+        val updated_timestamp: Long,
+        val status: String,
+        val version: Int = 1
+) {
+    companion object {
+        const val KEY_USER_DOC_ID = "userDocId"
+    }
+}
+
 data class UserDoc(
         var uid: String? = null,
         var firebase_uid: String? = null,
@@ -10,4 +21,20 @@ data class UserDoc(
         var status: String? = null,
         var email: String? = null,
         val version: Int = 1
-)
+) {
+
+    companion object {
+        const val KEY_UID = "uid"
+        const val KEY_FIREBASE_UID = "firebase_uid"
+        const val KEY_FIREFOX_UID = "firefox_uid"
+        const val KEY_CREATED_TIMESTAMP = "created_timestamp"
+        const val KEY_UPDATED_TIMESTAMP = "updated_timestamp"
+        const val KEY_STATUS = "status"
+        const val KEY_EMAIL = "email"
+        const val KEY_VERSION = "version"
+
+        const val STATUS_SIGN_IN = "sign-in"
+        const val STATUS_DEPRECATED = "deprecated"
+        const val STATUS_SUSPEND = "suspend"
+    }
+}
