5f560df58ec2764bbf588088c4cfb485fd5cab4e
Nevin Chen <cnevinchen@gmail.com>
2019-09-25 15:57:25 +0800

    Login Front-end

---
+++ src/main/java/org/mozilla/msrp/platform/vertical/admin/model/SearchCriteria.java
@@ -0,0 +1,16 @@
+package org.mozilla.msrp.platform.vertical.admin.model;
+
+
+
+public class SearchCriteria {
+
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
