dcc00c00e30b147d4a2650e36efd29862a91557a
Nevin Chen <cnevinchen@gmail.com>
2019-09-25 19:07:24 +0800

    Add Games API

---
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/data/Items.kt
@@ -0,0 +1,9 @@
+package org.mozilla.msrp.platform.vertical.content.data
+
+data class Item(
+        val source: String,
+        val imageUrl: String,
+        val linkUrl: String,
+        val title: String,
+        val componentId: String
+)
