9e4ed90b7adc84bfb2f17164e88f11e3bc53038d
Nevin Chen <cnevinchen@gmail.com>
2019-09-25 19:07:24 +0800

    Add Games API

---
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/ContentRepository.kt
@@ -0,0 +1,38 @@
+package org.mozilla.msrp.platform.vertical.content
+
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.Storage
+import com.google.cloud.storage.StorageException
+import org.mozilla.msrp.platform.util.logger
+import org.springframework.stereotype.Repository
+import java.nio.charset.StandardCharsets.UTF_8
+import javax.inject.Inject
+
+@Repository
+class ContentRepository @Inject constructor(private var storage: Storage) {
+
+    private val log = logger()
+
+    fun getContent(contentRepoQuery: ContentRepoQuery): ContentRepoResult {
+        return try {
+            val path = "v1/${contentRepoQuery.category}/${contentRepoQuery.languageLocale}/"
+            val blobId = BlobId.of("rocket-admin-dev", "${path}game_mock_items.json")
+            val bytes = storage.readAllBytes(blobId)
+            ContentRepoResult.Success(String(bytes, UTF_8))
+        } catch (e: StorageException) {
+            val message = "error loading games"
+            log.error("[Games]====$message:${e.localizedMessage}")
+            ContentRepoResult.Fail(message)
+        }
+    }
+}
+
+sealed class ContentRepoResult {
+    class Success(val result: String) : ContentRepoResult()
+    class Fail(val message: String) : ContentRepoResult()
+}
+
+data class ContentRepoQuery(
+        val category: String,
+        val languageLocale: String
+)
---
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/GameController.kt
@@ -0,0 +1,68 @@
+package org.mozilla.msrp.platform.vertical.content
+
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import org.mozilla.msrp.platform.util.logger
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+
+@RestController
+class GameController @Inject constructor(private val contentRepository: ContentRepository) {
+
+    private val log = logger()
+
+    private val cacheGames = CacheBuilder.newBuilder()
+            .maximumSize(1000)
+            .refreshAfterWrite(15, TimeUnit.MINUTES)
+            .recordStats()
+            .build(
+                    object : CacheLoader<ContentRepoQuery, ContentRepoResult>() {
+                        override fun load(key: ContentRepoQuery): ContentRepoResult {
+                            return contentRepository.getContent(key)
+                        }
+                    })
+
+    private val categoryMapping = hashMapOf(
+            "html5" to "games_html5",
+            "install" to "games_install")
+    private val supportLanguageLocale = listOf("id-ID", "en-IN")
+
+    @GetMapping("/api/v1/games/{category}")
+    internal fun games(
+            @PathVariable(value = "category") category: String,
+            @RequestParam(value = "language") language: String,
+            @RequestParam(value = "country") country: String): ResponseEntity<String> {
+
+        val safeCategory = categoryMapping[category]
+        if (safeCategory == null || !supportLanguageLocale.contains(language)) {
+            val message = "Not supported parameters for games: $category/$language/$country"
+            log.warn("[Games]====$message")
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message)
+        }
+
+        try {
+            return when (val result = cacheGames.get(ContentRepoQuery(safeCategory, language))) {
+                is ContentRepoResult.Success -> {
+                    ResponseEntity.ok(result.result)
+                }
+                is ContentRepoResult.Fail -> {
+                    val message = "Can't find games for $category/$language/$country"
+                    log.warn("[Games]====$message")
+                    ResponseEntity.status(HttpStatus.NOT_FOUND).body(result.message)
+                }
+            }
+        } catch (e: ExecutionException) {
+            val message = "error loading games"
+            log.error("[Games]====$message, exception=$e")
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message)
+        }
+    }
+}
