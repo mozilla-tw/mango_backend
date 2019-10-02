71fb573597c7dfe2ccbdacfe0168c42284c5e2c7
Nevin Chen <cnevinchen@gmail.com>
2019-10-02 11:51:50 +0800

    Change log for bean creation and make devtool happy

--- src/main/java/org/mozilla/msrp/platform/user/FirefoxAccountClientConfiguration.java
+++ src/main/java/org/mozilla/msrp/platform/user/FirefoxAccountClientConfiguration.java
@@ -1,5 +1,6 @@
 package org.mozilla.msrp.platform.user;
 
+import lombok.extern.log4j.Log4j2;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,15 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import javax.inject.Named;
 
+@Log4j2
 @Named
 public class FirefoxAccountClientConfiguration {
 
 
     @Bean("FxaAuth")
     public FirefoxAccountClient authFirefoxAccountClient() {
+        log.info(" --- Bean Creation FxaAuth ---");
+
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
