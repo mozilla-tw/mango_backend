ef01405317e6787a66c55c0fb7d48b61758e63b3
Nevin Chen <cnevinchen@gmail.com>
2019-10-03 13:18:24 +0800

    Fix bug for upload reward

--- src/main/java/org/mozilla/msrp/platform/redward/RewardData.kt
+++ src/main/java/org/mozilla/msrp/platform/redward/RewardData.kt
@@ -3,21 +3,10 @@ package org.mozilla.msrp.platform.redward
 
 class RewardCouponDoc(
     var rid: String? = null,
-    var uid: String? = null,
+    var uid: String = "",
     var mid: String? = null,
     var code: String? = null,
     var expire_date: Long? = null,
     var created_timestamp: Long? = null,
     var updated_timestamp: Long? = null
 )
-
-
-
-val testReward = RewardCouponDoc(
-    rid = "lZuctiWQZcC4F6oo4Zj2",
-    uid = "1111",
-    mid = "22222",
-    code = "C0UP0N2019",
-    expire_date = 1567756983,
-    created_timestamp = 3333333,
-    updated_timestamp = 3333333)
