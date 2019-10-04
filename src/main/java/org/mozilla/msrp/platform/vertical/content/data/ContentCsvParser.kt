7ccd449469b172e89aaa081aa9988127066087bb
Nevin Chen <cnevinchen@gmail.com>
2019-10-04 17:14:42 +0800

    Send all CSV data to client
    The client will need different kinds of data (game/shopping/travel) from currater.
    It's better the server send all datat to the client.
    So the client can decide what to do with that data instead of changing the server api.
    
    But the server will still need to check the data validity. See #84

---
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/data/ContentCsvParser.kt
@@ -0,0 +1,67 @@
+package org.mozilla.msrp.platform.vertical.content.data
+
+import com.opencsv.bean.CsvToBeanBuilder
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
+
+object CsvParserConfig {
+    const val BANNER_SUBCATEGORY_NAME = "banner"
+    const val BANNER_SUBCATEGORY_TYPE = "banner"
+    const val BANNER_SUBCATEGORY_ID = 4
+}
+
+fun parseContent(version: Int, bannerBytes: ByteArray?, listItemBytes: ByteArray): ContentResponse {
+
+    val subCategories = mutableListOf<ContentSubcategory>()
+
+
+    // add banner Subcategory
+    val bannerSubCategory = parseBanner(bannerBytes)
+    if (bannerSubCategory != null) {
+        subCategories.add(bannerSubCategory)
+    }
+
+    // get other Subcategory
+    val otherSubcategory = parseOthers(listItemBytes)
+
+    subCategories.addAll(otherSubcategory)
+
+    return ContentResponse(version, subCategories)
+}
+
+private fun parseOthers(bytes: ByteArray): List<ContentSubcategory> {
+    val csvEntryList = CsvToBeanBuilder<ContentItem>(InputStreamReader(ByteArrayInputStream(bytes))).withType(ContentItem::class.java).build().parse()
+
+    // collect sub-categories
+    val subCategorySet = linkedSetOf<ContentSubcategory>()
+    for (entry in csvEntryList) {
+        val componentTypeName: String = entry.component_type_name ?: break
+        val subcategoryName = entry.subcategory_name ?: break
+        val subcategoryId = entry.subcategory_id ?: break
+
+        subCategorySet.add(ContentSubcategory(componentTypeName, subcategoryName, subcategoryId, listOf()))
+    }
+
+    // for each sub-categories
+    for (subcategory in subCategorySet) {
+        val otherItems = mutableListOf<ContentItem>()
+
+        for (content in csvEntryList) {
+            if (content.subcategory_id == subcategory.subcategoryId) {
+                otherItems.add(content)
+            }
+        }
+        subcategory.items = otherItems
+    }
+    return subCategorySet.toList()
+}
+
+private fun parseBanner(bytes: ByteArray?): ContentSubcategory? {
+    if (bytes == null) return null
+    val bannerItems = mutableListOf<ContentItem>()
+    val csvEntryList = CsvToBeanBuilder<ContentItem>(InputStreamReader(ByteArrayInputStream(bytes))).withType(ContentItem::class.java).build().parse()
+    for (content in csvEntryList) {
+        bannerItems.add(content)
+    }
+    return ContentSubcategory(CsvParserConfig.BANNER_SUBCATEGORY_NAME, CsvParserConfig.BANNER_SUBCATEGORY_TYPE, CsvParserConfig.BANNER_SUBCATEGORY_ID, bannerItems)
+}
---
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/data/ContentItem.kt
@@ -0,0 +1,35 @@
+package org.mozilla.msrp.platform.vertical.content.data
+
+import com.opencsv.bean.CsvBindByName
+import org.mozilla.msrp.platform.util.hash
+
+// TODO: set the required fields for each categories
+//  https://docs.google.com/spreadsheets/d/1s--x8TiIsEEISHl3YJA8ulAljxJt4FQUHZ0r5tCxHy0/edit#gid=1268475188
+class ContentItem(
+        @field:CsvBindByName(required = false) var created_at: Long? = 0L,
+        @field:CsvBindByName(required = false) var country: String? = "",
+        @field:CsvBindByName(required = false) var source_name: String? = "",
+        @field:CsvBindByName(required = false) var source_type: String? = "",
+        @field:CsvBindByName(required = false) var partner: Boolean? = false,
+        @field:CsvBindByName(required = false) var vertical_name: String? = "",
+        @field:CsvBindByName(required = false) var vertical_id: Int? = 0,
+        @field:CsvBindByName(required = false) var category_name: String? = "",
+        @field:CsvBindByName(required = false) var category_id: Int? = 0,
+        @field:CsvBindByName(required = false) var subcategory_name: String? = "",
+        @field:CsvBindByName(required = false) var subcategory_id: Int? = 0,
+        @field:CsvBindByName(required = false) var component_type_name: String? = "",
+        @field:CsvBindByName(required = false) var component_type_id: Int? = 0,
+        @field:CsvBindByName(required = false) var image_type: String? = "",
+        @field:CsvBindByName(required = false) var image: String? = "",
+        @field:CsvBindByName(required = false) var destination: String = "",
+        @field:CsvBindByName(required = false) var title: String? = "",
+        @field:CsvBindByName(required = false) var description: String? = "",
+        @field:CsvBindByName(required = false) var score: Double? = 0.0,
+        @field:CsvBindByName(required = false) var score_reviews: Int? = 0,
+        @field:CsvBindByName(required = false) var price: String? = "",
+        @field:CsvBindByName(required = false) var discount: String? = "",
+        @field:CsvBindByName(required = false) var fresh: Boolean? = false,
+        @field:CsvBindByName(required = false) var start_date: Long? = 0L,
+        @field:CsvBindByName(required = false) var end_date: Long? = 0L,
+        var component_id: String = destination.hash()
+)
