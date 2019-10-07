f803cb06f0acffbb6445f103bab8ce6a110fa070
Nevin Chen <cnevinchen@gmail.com>
2019-10-07 19:10:43 +0800

    Add publish version.

--- src/main/java/org/mozilla/msrp/platform/vertical/content/ContentController.kt
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/ContentController.kt
@@ -3,6 +3,7 @@ package org.mozilla.msrp.platform.vertical.content
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.mozilla.msrp.platform.common.auth.JwtHelper
 import org.mozilla.msrp.platform.util.logger
+import org.mozilla.msrp.platform.vertical.content.data.ContentSubcategory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -29,7 +30,7 @@ class ContentController @Inject constructor(private val contentService: ContentS
     ): ResponseEntity<Any> {
         return when (val result = contentService.getContent(category, locale)) {
             is ContentServiceQueryResult.InvalidParam -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.message)
-            is ContentServiceQueryResult.Success -> ResponseEntity.status(HttpStatus.OK).body(result.category)
+            is ContentServiceQueryResult.Success -> ResponseEntity.status(HttpStatus.OK).body(ContentResponse(result.version, result.tag, result.data.subcategories))
             is ContentServiceQueryResult.Fail -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result.message)
         }
     }
@@ -38,18 +39,16 @@ class ContentController @Inject constructor(private val contentService: ContentS
     @GetMapping("/api/v1/content/publish")
     fun publishContent(
             @RequestParam token: String,
-            @RequestParam category: String,
-            @RequestParam locale: String,
             @RequestParam publishDocId: String,
-            @RequestParam editorUid: String,
-            @RequestParam(required = false) schedule: String = ""
+            @RequestParam editor: String,
+            @RequestParam(required = false) schedule: String? = ""
     ): ResponseEntity<String> {
         if (JwtHelper.verify(token)?.role != JwtHelper.ROLE_PUBLISH_ADMIN) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("No Permission")
         }
-        return when (val result = contentService.publish(category, locale, publishDocId, editorUid, schedule)) {
-            ContentServicePublishResult.Done -> ResponseEntity.status(HttpStatus.OK).body("<a href='../content?category=$category&locale=$locale'>preview</a>")
-            is ContentServicePublishResult.InvalidParam -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.message)
+        return when (val result = contentService.publish(publishDocId, editor, schedule)) {
+            is ContentServicePublishResult.Success -> ResponseEntity.status(HttpStatus.OK).body("<a href='../content?category=${result.category}&locale=${result.locale}'>View Client JSON</a>")
+            is ContentServicePublishResult.Fail -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result.message)
         }
     }
 
@@ -71,7 +70,7 @@ class ContentController @Inject constructor(private val contentService: ContentS
         return when (val result = contentService.uploadContent(category, locale, other, banner, tag)) {
             is ContentServiceUploadResult.Success -> {
                 val preview = "/api/v1/admin/publish/preview?token=$token&publishDocId=${result.publishDocId}"
-                val publish = "/api/v1/content/publish?token=$token&category=$category&locale=$locale&publishDocId=${result.publishDocId}&editorUid=${verify.email}"
+                val publish = "/api/v1/content/publish?token=$token&category=$category&locale=$locale&publishDocId=${result.publishDocId}&editor=${verify.email}"
                 ResponseEntity.status(HttpStatus.OK).body("" +
                         "<a href='$preview'>Preview</a> <BR> " +
                         "<a href='$publish'>Publish Now</a><BR> " +
@@ -80,7 +79,7 @@ class ContentController @Inject constructor(private val contentService: ContentS
                         "<input type='hidden' name='category' value='$category'>" +
                         "<input type='hidden' name='locale' value='$locale'>" +
                         "<input type='hidden' name='publishDocId' value='${result.publishDocId}'>" +
-                        "<input type='hidden' name='editorUid' value='${verify.email}'>" +
+                        "<input type='hidden' name='editor' value='${verify.email}'>" +
 
                         "<input type='date' name='schedule'>" +
                         "<input type='submit' value='Schedule Publish'>" +
@@ -95,3 +94,9 @@ class ContentController @Inject constructor(private val contentService: ContentS
     // ======================== ADMIN ======================== END
 
 }
+
+class ContentResponse(
+        val version: Long,
+        val tag: String,
+        val subcategories: List<ContentSubcategory>
+)
--- src/main/java/org/mozilla/msrp/platform/vertical/content/data/Category.kt
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/data/Category.kt
@@ -1,6 +1,5 @@
 package org.mozilla.msrp.platform.vertical.content.data
 
 data class Category(
-        var subcategories: List<ContentSubcategory> = listOf(),
-        var version: Int = 1
+        var subcategories: List<ContentSubcategory> = listOf()
 )
