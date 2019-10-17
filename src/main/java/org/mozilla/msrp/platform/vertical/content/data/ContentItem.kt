ed9a57911a261bc641886c91e0d8903f2e745f0b
Nevin <cnevinchen@gmail.com>
2019-10-17 16:54:14 +0800

    Nevin/shopping data (#128)
    * Add Addtional
    
    * Fix component_id all the same issue
    
    * Make score_reviews as String

--- src/main/java/org/mozilla/msrp/platform/vertical/content/data/ContentItem.kt
+++ src/main/java/org/mozilla/msrp/platform/vertical/content/data/ContentItem.kt
@@ -1,7 +1,6 @@
 package org.mozilla.msrp.platform.vertical.content.data
 
 import com.opencsv.bean.CsvBindByName
-import org.mozilla.msrp.platform.util.hash
 
 // TODO: set the required fields for each categories
 //  https://docs.google.com/spreadsheets/d/1s--x8TiIsEEISHl3YJA8ulAljxJt4FQUHZ0r5tCxHy0/edit#gid=1268475188
@@ -25,11 +24,12 @@ class ContentItem(
         @field:CsvBindByName(required = false) var title: String? = "",
         @field:CsvBindByName(required = false) var description: String? = "",
         @field:CsvBindByName(required = false) var score: Double? = 0.0,
-        @field:CsvBindByName(required = false) var score_reviews: Int? = 0,
+        @field:CsvBindByName(required = false) var score_reviews: String? = "",
         @field:CsvBindByName(required = false) var price: String? = "",
         @field:CsvBindByName(required = false) var discount: String? = "",
         @field:CsvBindByName(required = false) var fresh: Boolean? = false,
         @field:CsvBindByName(required = false) var start_date: Long? = 0L,
         @field:CsvBindByName(required = false) var end_date: Long? = 0L,
-        var component_id: String = destination.hash()
+        @field:CsvBindByName(required = false) var additional: String = "",
+        var component_id: String = ""
 )
