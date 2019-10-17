820bcb6889f9c1fe75404543207138a3f1bd6578
Roger <pharspher@gmail.com>
2019-10-17 17:11:12 +0800

    Add reason code to join api error response

--- src/main/java/org/mozilla/msrp/platform/mission/MissionJoinResponse.kt
+++ src/main/java/org/mozilla/msrp/platform/mission/MissionJoinResponse.kt
@@ -4,10 +4,19 @@ import org.springframework.http.HttpStatus
 
 sealed class MissionJoinResponse {
     class Success(val result: MissionJoinResult) : MissionJoinResponse()
-    class Error(val message: String) : MissionJoinResponse()
+    class Error(val message: String, val reason: Int) : MissionJoinResponse()
 }
 
 sealed class MissionJoinResult {
     class Success(val mid: String, val status: JoinStatus) : MissionJoinResult()
-    class Error(val message: String, val code: HttpStatus) : MissionJoinResult()
+    class Error(val message: String, val code: HttpStatus, val reason: JoinFailedReason) : MissionJoinResult()
+}
+
+enum class JoinFailedReason(val code: Int) {
+    Unknown(0),
+    NotOpen(1),
+    Closed(2),
+    AlreadyJoined(3),
+    NoQuota(4),
+    NotExist(5)
 }
