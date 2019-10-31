1136d67f35dab2d7484eb5c192424d1c1071e9a8
Nevin Chen <cnevinchen@gmail.com>
2019-10-31 19:12:05 +0800

    Seed should be the same across all instances

--- src/main/java/org/mozilla/msrp/platform/admin/AdminMsrpController.kt
+++ src/main/java/org/mozilla/msrp/platform/admin/AdminMsrpController.kt
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import javax.inject.Inject
 
 
 @Controller
-class AdminMsrpController {
+class AdminMsrpController @Inject constructor(private val jwtHelper: JwtHelper) {
 
     @GetMapping("/api/v1/admin/msrp")
     fun adminMSRP(@RequestParam token: String, model: Model): String {
-        val role = JwtHelper.verify(token)?.role
+        val role = jwtHelper.verify(token)?.role
         if (role == JwtHelper.ROLE_MSRP_ADMIN) {
             model.addAttribute("token", token)
             return "msrp"
--- src/main/java/org/mozilla/msrp/platform/common/auth/AuthUtils.kt
+++ src/main/java/org/mozilla/msrp/platform/common/auth/AuthUtils.kt
@@ -4,28 +4,33 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.exceptions.JWTCreationException
 import com.auth0.jwt.exceptions.JWTVerificationException
+import org.mozilla.msrp.platform.user.FirefoxAccountServiceInfo
 import org.mozilla.msrp.platform.util.logger
 import java.util.Date
-import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Named
 
-object JwtHelper {
+@Named
+class JwtHelper @Inject constructor(firefoxAccountServiceInfo: FirefoxAccountServiceInfo) {
 
-    const val ROLE_PUBLISH_ADMIN = "publish_admin"
-    const val ROLE_MSRP_ADMIN = "msrp_admin"
-
-    private const val ISSUER = "org.mozilla.msrp"
-    private const val ROLE = "role"
-    private const val EMAIL = "email"
-    private const val EXPIRATION = 30 * 60 * 1000  // expired in 50 minutes
-
-    private val SECRET = UUID.randomUUID().toString()   // the secret is different every time the server starts
+    private val SECRET = firefoxAccountServiceInfo.clientSecret   // the secret is must be the same
     private val algorithm = Algorithm.HMAC256(SECRET)
     private val verifier = JWT.require(algorithm).withIssuer(ISSUER).build()
     private val log = logger()
 
-    @JvmStatic
+    companion object{
+        const val ROLE_PUBLISH_ADMIN = "publish_admin"
+        const val ROLE_MSRP_ADMIN = "msrp_admin"
+
+        private const val ISSUER = "org.mozilla.msrp"
+        private const val ROLE = "role"
+        private const val EMAIL = "email"
+        private const val EXPIRATION = 30 * 60 * 1000  // expired in 50 minutes
+    }
+
     fun createToken(role: String, email: String): String? {
         try {
+
             val algorithm = Algorithm.HMAC256(SECRET)
             return JWT.create()
                     .withExpiresAt(Date(System.currentTimeMillis() + EXPIRATION))
@@ -40,7 +45,6 @@ object JwtHelper {
         }
     }
 
-    @JvmStatic
     fun verify(token: String): Auth? {
         return try {
             return verifier.verify(token).let {
