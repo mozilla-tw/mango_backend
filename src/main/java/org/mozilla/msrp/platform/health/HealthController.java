8de8d77ce4fa7a4b6536d4b4d2b1794d5be72758
Nevin Chen <cnevinchen@gmail.com>
2019-11-28 16:36:06 +0800

    Add test alert

--- src/main/java/org/mozilla/msrp/platform/health/HealthController.java
+++ src/main/java/org/mozilla/msrp/platform/health/HealthController.java
@@ -1,8 +1,8 @@
 package org.mozilla.msrp.platform.health;
 
+import org.mozilla.msrp.platform.metrics.Metrics;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +11,45 @@ public class HealthController {
     @GetMapping("/health")
     ResponseEntity isHealthy() {
         return ResponseEntity.ok("healthy");
+    }
+
+
+    @GetMapping("/EVENT_USER_SUSPENDED")
+    ResponseEntity EVENT_USER_SUSPENDED() {
+        Metrics.event(Metrics.EVENT_USER_SUSPENDED, "");
+        return ResponseEntity.ok("EVENT_USER_SUSPENDED");
+    }
+
+
+    @GetMapping("/EVENT_USER_BIND_FAIL")
+    ResponseEntity EVENT_USER_BIND_FAIL() {
+        Metrics.event(Metrics.EVENT_USER_BIND_FAIL, "");
+        return ResponseEntity.ok("EVENT_USER_BIND_FAIL");
+    }
+
+    @GetMapping("/EVENT_REDEEM_FAIL")
+    ResponseEntity EVENT_REDEEM_FAIL() {
+        Metrics.event(Metrics.EVENT_REDEEM_FAIL, "");
+        return ResponseEntity.ok("EVENT_REDEEM_FAIL");
+    }
+
+
+    @GetMapping("/EVENT_REDEEM_CONSUMED")
+    ResponseEntity EVENT_REDEEM_CONSUMED() {
+        Metrics.event(Metrics.EVENT_REDEEM_CONSUMED, "");
+        return ResponseEntity.ok("EVENT_REDEEM_CONSUMED");
+    }
+
+
+    @GetMapping("/EVENT_MISSION_JOINED")
+    ResponseEntity EVENT_MISSION_JOINED() {
+        Metrics.event(Metrics.EVENT_MISSION_JOINED, "");
+        return ResponseEntity.ok("EVENT_MISSION_JOINED");
+    }
+
+    @GetMapping("/EVENT_MISSION_CHECK_IN")
+    ResponseEntity EVENT_MISSION_CHECK_IN() {
+        Metrics.event(Metrics.EVENT_MISSION_CHECK_IN, "");
+        return ResponseEntity.ok("EVENT_MISSION_CHECK_IN");
     }
 }
