fd85dc57b8d3665bce3bd838f50903cecc47db11
Nevin Chen <cnevinchen@gmail.com>
2020-01-05 23:46:03 +0800

    Add log when Youtube API error

--- src/main/java/org/mozilla/msrp/platform/metrics/VerticalMetrics.kt
+++ src/main/java/org/mozilla/msrp/platform/metrics/VerticalMetrics.kt
@@ -7,7 +7,8 @@ class VerticalMetrics {
     companion object {
         const val EVENT_CACHE_MISSED = "cache_missed"
         const val EVENT_CACHE_EXPIRED = "cache_expired"
-        const val EVENT_CACHE_EXCEPTION= "cache_exception"
+        const val EVENT_CACHE_EXCEPTION = "cache_exception"
+        const val EVENT_YOUTUBE_API_ERROR = "youtube_api_error"
 
         @JvmStatic
         fun event(key: String, value: String = "") {
