808ce750d45d58e10a8f5a244e0d648adfa03f88
Nevin Chen <cnevinchen@gmail.com>
2020-03-03 16:30:58 +0800

    Add FCM request POJO

--- src/main/java/org/mozilla/msrp/platform/push/AdminPushController.kt
+++ src/main/java/org/mozilla/msrp/platform/push/AdminPushController.kt
@@ -1,6 +1,5 @@
 package org.mozilla.msrp.platform.push
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.core.ApiFuture
 import com.google.api.core.ApiFutures
 import com.google.cloud.ServiceOptions
@@ -17,10 +16,6 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.io.FileInputStream
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.*
 import javax.inject.Inject
 
@@ -34,7 +29,7 @@ class AdminPushController @Inject constructor(private val jwtHelper: JwtHelper) 
      * Show Push ADMIN UI to verified users
      * */
     @GetMapping("/api/v1/admin/push/main")
-    fun adminPush(@RequestParam messageCount: Int, model: Model): String {
+    fun adminPush(@RequestParam token: String, model: Model): String {
 //        val role = jwtHelper.verify(token)?.role
 //        if (role == JwtHelper.ROLE_MSRP_ADMIN) {
 //            model.addAttribute("token", token)
@@ -46,17 +41,50 @@ class AdminPushController @Inject constructor(private val jwtHelper: JwtHelper) 
      * Receive message created from Frontend
      * */
     @GetMapping("/api/v1/admin/push/enqueue")
-    fun enqueuedMessageRequest(@RequestParam messageCount: Int, model: Model): String {
+    fun enqueuedMessageRequest(
+            @RequestParam token: String,
+            @RequestParam(value = "recipients[]") recipients: Array<String>,
+            @RequestParam recipientType: String,
+            @RequestParam title: String,
+            @RequestParam body: String,
+            @RequestParam destination: String,
+            @RequestParam displayType: String,
+            @RequestParam displayTimestamp: Long,
+            @RequestParam mozMessageId: String,
+            @RequestParam appId: String,
+            @RequestParam(required = false, defaultValue = "") imageUrl: String): String {
+        val verify = jwtHelper.verify(token)
+        if (verify?.role != JwtHelper.ROLE_PUSH_ADMIN) {
+            return "401"
+        }
+        // FIXME: consider  checking the input here.
 
+        val sender = verify.email
         val topicId = "push"
         val topicName = ProjectTopicName.of(PROJECT_ID, topicId)
         var publisher: Publisher? = null
         val futures: MutableList<ApiFuture<String>> = ArrayList()
 
+        // TODO: write log to DB
         try { // Create a publisher instance with default settings bound to the topic
             publisher = Publisher.newBuilder(topicName).build()
-            for (i in 0 until messageCount) {
-                val message = PushNotificationHelper.conductPushPayload("token_and_other_para$i")
+            for (recipient in recipients) {
+                val recipientToken = recipient  // TODO: add exchange here
+                // TODO: Make it a POJO. Log DB will use it too. consider JSON from f2e too
+                val fcmRequest = FcmRequest(
+                        recipientToken = recipientToken,
+                        title = title,
+                        body = body,
+                        destination = destination,
+                        displayType = displayType,
+                        displayTimestamp = displayTimestamp,
+                        mozMessageId = mozMessageId,
+                        appId = appId,
+                        imageUrl = imageUrl,
+                        sender = sender
+
+                )
+                val message = PushNotificationHelper.conductPushPayload(fcmRequest)
 
                 // convert message to bytes
                 val data: ByteString = ByteString.copyFromUtf8(message)
@@ -66,32 +94,57 @@ class AdminPushController @Inject constructor(private val jwtHelper: JwtHelper) 
                 // Schedule a message to be published. Messages are automatically batched.
                 val future: ApiFuture<String> = publisher.publish(pubsubMessage)
                 futures.add(future)
+                // FIXME: consider the error logging here
             }
+            // FIXME: consider the error logging here
+        } catch (e: Exception) {
+            // FIXME: consider the error logging here
         } finally { // Wait on any pending requests
+            // FIXME logging one by one?
             val messageIds = ApiFutures.allAsList(futures).get()
-            for (messageId in messageIds) {
-                println(messageId)
+            // TODO: write log to DB
+            if (messageIds.size != recipients.size) {
+                // FIXME: DO we want to rollback the queue here?
+                return " Expecting ${recipients.size} FCM request enqueued, but got ${messageIds.size}"
             }
-            if (publisher != null) { // When finished with the publisher, shutdown to free up resources.
-                publisher.shutdown()
-            }
+            // FIXME: consider the error logging here
+            publisher?.shutdown()
         }
-        return "$messageCount messages sent!"
+        return " messages sent!"
     }
 }
 
+data class FcmRequest(
+        val recipientToken: String,
+        val title: String,
+        val body: String,
+        val destination: String,
+        val displayType: String,
+        val displayTimestamp: Long,
+        val mozMessageId: String,
+        val appId: String,
+        val imageUrl: String,
+        val sender: String,
+        val pushId: String = UUID.randomUUID().toString(),
+        val createdTimestamp: Long = System.currentTimeMillis()
+)
 
 object PushNotificationHelper {
     private val mapper = Gson()
-
-    @Throws(IOException::class)
-    fun conductPushPayload(deviceToken: String?): String {
-        val fcmOptions = FcmOptions.builder().setAnalyticsLabel("MyLabel").build()
+    fun conductPushPayload(
+            fcmRequest: FcmRequest): String {
+        val fcmOptions = FcmOptions.builder().setAnalyticsLabel(fcmRequest.mozMessageId).build()
         val push = Message.builder()
-                .putData("title", "notification title")
-                .putData("body", "notification body")
-                .setFcmOptions(fcmOptions)
-                .setToken(deviceToken).build()
+                .putData("body", fcmRequest.body)
+                .putData("title", fcmRequest.title)
+                .putData("appId", fcmRequest.appId)
+                .putData("destination", fcmRequest.destination)
+                .putData("displayType", fcmRequest.displayType)
+                .putData("imageUrl", fcmRequest.imageUrl)
+                .putData("pushId", fcmRequest.pushId)
+                .putData("recipientToken", fcmRequest.recipientToken)
+                .putData("sender", fcmRequest.sender)
+                .setFcmOptions(fcmOptions).build()
         return mapper.toJson(PushPayload(push))
     }
 }
