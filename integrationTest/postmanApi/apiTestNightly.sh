9dd93d45a1e3b107d3b0f7cfbfa12bd6323bc690
Daisy-pliu <pliu@mozilla.com>
2020-05-25 15:11:39 +0800

    fix nightly tests as folder reorg

--- integrationTest/postmanApi/apiTestNightly.sh
+++ integrationTest/postmanApi/apiTestNightly.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # API test on News 
-newman run postmanApi/collection/VERTICAL.json --folder NEWS -d postmanApi/data/news_data.json -e postmanApi/environment/VERTICAL_NIGHTLY.json
+newman run integrationTest/postmanApi/collection/VERTICAL.json --folder NEWS -d integrationTest/postmanApi/data/news_data.json -e integrationTest/postmanApi/environment/VERTICAL_NIGHTLY.json
 # API Test on Game 
-newman run postmanApi/collection/VERTICAL.json --folder GAME -e postmanApi/environment/VERTICAL_NIGHTLY.json
+newman run integrationTest/postmanApi/collection/VERTICAL.json --folder GAME -e integrationTest/postmanApi/environment/VERTICAL_NIGHTLY.json
 # API Test on Travel
-newman run postmanApi/collection/VERTICAL.json --folder TRAVEL -e postmanApi/environment/VERTICAL_NIGHTLY.json
+newman run integrationTest/postmanApi/collection/VERTICAL.json --folder TRAVEL -e integrationTest/postmanApi/environment/VERTICAL_NIGHTLY.json
 # API Test on Shopping
-newman run postmanApi/collection/VERTICAL.json --folder SHOPPING -e postmanApi/environment/VERTICAL_NIGHTLY.json
+newman run integrationTest/postmanApi/collection/VERTICAL.json --folder SHOPPING -e integrationTest/postmanApi/environment/VERTICAL_NIGHTLY.json
